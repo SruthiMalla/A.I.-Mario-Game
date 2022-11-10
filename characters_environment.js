b6e5442eec2ab78d4da437086b98f4608ff737ff
SruthiMalla <52434141+SruthiMalla@users.noreply.github.com>
2022-11-10 16:39:54 -0500

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -77,7 +77,7 @@ function game()
     fill(255, 255, 255);
     textSize(40);
     textAlign(CENTER);
-    text("Press Any Arrow Keys to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
+    text("Press Play Button to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
     textSize(40);
 
     stroke(255);
@@ -125,7 +125,9 @@ function game()
 
 // change game status if any key is pressed
 function changeGameStatud(character){
-  if((keyDown(control.up) ||keyDown(control.left)||keyDown(control.right) )&& gameConfig.status==="start") {
+  if(GameStatus == "start" && noseX!= "" && gameConfig.status==="start") 
+  {
+    document.getElementById("status").innerHTML = "Game is Loaded";
     world_start.play();
     initializeCharacterStatus(mario);
     gameConfig.status= "play";
@@ -288,14 +290,16 @@ function autoControl(character){
 /* Manual control character */
 function manualControl(character){
   
-  if(character.live){
-    if(keyDown(control.left)){
+  if(character.live)
+  {
+    if(noseX < 300){
       character.velocity.x-=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(-1);
     }
 
-    if(keyDown(control.right)){
+    if(noseX > 300)
+    {
       character.velocity.x+=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(1);
@@ -310,7 +314,7 @@ function manualControl(character){
 
 /* Movements of character */
 function jumping(character){
-	if( (keyWentDown(control.up)&&character.live) || (touchIsDown&&character.live) ){
+	if( (noseY < 200 &&character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
 	}
 }
